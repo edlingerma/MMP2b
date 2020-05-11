091795722ce11279f86c9e11994a863eb8e4ad75
Maria Edlinger <medlinger.mmt-b2018@fh-salzburg.ac.at>
2020-05-11 16:27:48 +0200

    Fix: Entries are also destroyed on cascade

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,4 @@
 class Activity < ApplicationRecord
   belongs_to :challenge
-  has_many :entries, dependent: :delete_all
+  has_many :entries, dependent: :destroy
 end
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,7 +1,7 @@
 class Challenge < ApplicationRecord
   belongs_to :owner, class_name: 'User'
-  has_many :requests, dependent: :delete_all
+  has_many :requests, dependent: :destroy
   has_many :users, :through => :requests
-  has_many :activities, dependent: :delete_all
+  has_many :activities, dependent: :destroy
   accepts_nested_attributes_for :activities, allow_destroy: true, reject_if: proc { |attr| attr['title'].blank? }
 end
