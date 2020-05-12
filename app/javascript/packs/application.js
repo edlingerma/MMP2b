963762e6deef8d5315209418c87c55c4c20f6f50
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2020-05-12 14:24:23 +0200

    Add styling for show action

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,8 +16,6 @@ const context = require.context("../controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
 
-require('barChart')
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
