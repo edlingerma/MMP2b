337954b658db525f89eb6f3d63e7a6f293abbdd9
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2020-05-14 20:11:23 +0200

    HOTFIX add validation to ensure that goal is numerically

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,6 +4,7 @@ class Activity < ApplicationRecord
   validates :goal, :challenge, :title, presence: true
   validates_length_of :description, maximum: 350
   validates_length_of :goal, minimum: 1
+  validates_numericality_of :goal
   def amount
     entries.reduce(0) do |sum, entry|
       sum + entry.amount
