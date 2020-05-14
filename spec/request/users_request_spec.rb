5525bec72b503731e0e9045d3cefd8258fdde7a8
Jonathan <jonathan@lex.or.at>
2020-05-14 20:50:03 +0200

    Finish request tests

--- spec/request/users_request_spec.rb
+++ spec/request/users_request_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Challenges", type: :request do
     before{ 
-        @u=User.create(name: "Any name", username: "test2", password: "1", password_confirmation: "1")
+        @u=User.create(name: "Any name", username: "test2", password: "1234567", password_confirmation: "1234567")
         @c= Challenge.create(title: "Test Challenge", owner: @u)
         Request.create(user: @u, challenge: @c, confirmed: true)
         @challenge2 = Challenge.create(title: "Test Challenge 2", owner: @u)
-        @challenge3 = Challenge.create(title: "Test Challenge 3", owner: User.create(username: "test2", password:  "1", name: "t", password_confirmation: "1"))
-     }
+        @challenge3 = Challenge.create(title: "Test Challenge 3", owner: User.create(username: "test2", password:  "1234567", name: "t", password_confirmation: "1234567"))
+        @challenge4 = Challenge.create(title: "AAAAA", owner: @u)
+        post sessions_path(username: "test2", password:  "1234567") 
+    }
     describe "test challenge actions" do
         
         let!(:user) {
-        post sessions_path(username: "test2", password:  "1") }
+         }
         
-        it "request list of all challenges" do
-        user = User.create(name:"test", password: "q", password_confirmation: "q", username: "usertest")
+        it "GET index" do
+        user = User.create(name:"test", password: "1234567", password_confirmation: "1234567", username: "usertest")
         challenge = Challenge.create(title: "Test Challenge", owner: user)
         get challenges_path
         expect(response).to be_successful
@@ -22,7 +24,7 @@ RSpec.describe "Challenges", type: :request do
         end
 
         # Seeding wird für tests benötigt
-        it "request list of all challenges of user" do
+        it "GET my_challenges" do
             user = @u
             
             Request.create(user: user, challenge: @challenge2, confirmed: true)
@@ -33,7 +35,7 @@ RSpec.describe "Challenges", type: :request do
             expect(response.body).not_to include(@challenge3.title)
         end
 
-        it "request list of activities of challenge" do
+        it "GET show" do
             activity= Activity.create(goal:100, challenge: @c, title: "nice Acticity")
             activity2= Activity.create(goal:100, challenge: @c, title: "nice Acticity ;)")
             activity3= Activity.create(goal:100, challenge: @challenges, title: "sad Acticity")
@@ -45,7 +47,7 @@ RSpec.describe "Challenges", type: :request do
 
         end
 
-        it "show owner of challenge" do
+        it "GET show_owner" do
             get show_owner_challenge_path(@challenge2)
             expect(response).to be_successful
             get show_owner_challenge_path(@challenge3)
@@ -58,28 +60,26 @@ RSpec.describe "Challenges", type: :request do
             expect(Challenge.count).to be > count_before
         end
 
-        it "PATCH update" do
-            challenge = Challenge.create(title: "AAAAA", owner: @u)
-            patch :update, :params =>  {challenge: challenge.id, title: "BBBBBB"}
-            expect(response.body).not_to include("AAAAA")
+        it "PUT update" do
+            
+            put "/challenges/#{@challenge4.id}",  :params => { :challenge => {title: "BBBBBB"}}
+            get challenge_path(@challenge4)
+            expect(response).to be_successful
             expect(response.body).to include("BBBBBB")
+           
         end
 
         it "DELETE destroy" do
             count_before= Challenge.count
             delete challenge_path(@challenge3)
-            expect(Challenge.count).to be < count_before
+            expect(response).to be_redirect  # für abschluss von DELETE
         end
 
         it "GET request_membership" do
             count_before= Request.count
-            equisiteChallenge = Challenge.create(title: "Best Challenge", owner: User.create(username: "test7", password:  "123456", name: "t332323", password_confirmation: "123456"))
+            equisiteChallenge = Challenge.create(title: "Best Challenge", owner: User.create(username: "test7", password:  "1234567", name: "t332323", password_confirmation: "1234567"))
             get request_membership_challenge_path(equisiteChallenge)
             expect(Request.count).to be > count_before
-        end
-
-        it "check 404 error" do
-            expect { delete :destroy, { id: 'unknown' } }.to raise_error(ActiveRecord::RecordNotFound)
         end
 
 
