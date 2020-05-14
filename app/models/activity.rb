cfdb469b8843f50235cfe004a3b532be32c8c087
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2020-05-14 22:13:03 +0200

    Fix bug with validation of unit

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,6 +6,7 @@ class Activity < ApplicationRecord
   validates :goal, :challenge, :title, presence: true
   validates_length_of :description, maximum: 350
   validates_length_of :goal, minimum: 1
+  validates :unit, length: { maximum: 10 }
   validates_numericality_of :goal
   def amount
     entries.reduce(0) do |sum, entry|
