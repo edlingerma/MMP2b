f54fc9989fe9932c738ff2eba3f5cd36c008879f
Maria Edlinger <medlinger.mmt-b2018@fh-salzburg.ac.at>
2020-05-14 19:37:40 +0200

    Start with activity test

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -2,7 +2,16 @@
 
 require('rails_helper')
 RSpec.describe(Activity, type: :model) do
-  # test "the truth" do
-  #   assert true
-  # end
+  title = 'testtitle'
+  description = 'foo bar text'
+  goal = 100
+
+  describe "Create a new activity" 
+  
+    it('no new activity without goal') do
+      u = Activity.create( title: 'testtitle', description: 'foo bar text', goal: 100)
+      expect(u).to be_invalid
+    end
+    
+  end
 end
