39734e3b4172dcc5a41bba80e2c68da5980e7dd5
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2020-05-14 18:31:08 +0200

    Add Swagger for challenges api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :challenges do
     member do
       get :request_membership
---
+++ spec/requests/challenges_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require 'swagger_helper'
+
+RSpec.describe 'Challenges API', type: :request do
+  fixtures :challenges
+  before {
+    @challenge = challenge(:one)
+    @user = user(:one)
+  }
+  path '/challenges.json' do
+    get 'list all the challenges' do
+      produces 'application/json'
+
+      response(200, 'successful') do
+        schema type: :array,
+               items: {
+                   type: :object,
+                   properties: {
+                       id: { type: :integer },
+                       title: { type: :string },
+                       description: { type: :string },
+                       created_at: { type: :date },
+                       updated_at: { type: :date },
+                       owner: { type: :string },
+                       activities: {
+                           type: :array,
+                           items: {
+                               type: :object,
+                               properties: {
+                                   title: { type: :string },
+                                   description: { type: :string },
+                                   goal: { type: :integer },
+                                   progress: { type: :integer },
+                                   unit: { type: :string },
+                               },
+                           },
+                       },
+                       url: { type: :string }
+                   },
+               }
+
+        run_test!
+      end
+    end
+  end
+
+  path '/challenges/{id}.json' do
+    get 'show challenge' do
+      produces 'application/json'
+      parameter name: 'id', in: :path, type: :string
+
+      response(200, 'successful') do
+        schema type: :object,
+               properties: {
+                   id: { type: :integer },
+                   title: { type: :string },
+                   description: { type: :string },
+                   created_at: { type: :date },
+                   updated_at: { type: :date },
+                   owner: { type: :string },
+                   activities: {
+                       type: :array,
+                       items: {
+                           type: :object,
+                           properties: {
+                               title: { type: :string },
+                               description: { type: :string },
+                               goal: { type: :integer },
+                               progress: { type: :integer },
+                               unit: { type: :string },
+                           },
+                       },
+                   },
+                   url: { type: :string }
+               }
+
+        let(:id) do
+          c = Challenge.create!(title: 'Laufverein Elsbethen', description: 'Jeder ist willkommen!', owner: @user)
+          c.id
+        end
+
+        run_test!
+      end
+    end
+  end
+end
