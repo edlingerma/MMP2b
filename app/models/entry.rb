8dd36b086beca6af5948798ac7943b5d906c1e17
Markus Wallner <mwallner.mmt-b2018@fh-salzburg.ac.at>
2020-05-15 12:37:07 +0200

    HOTFIX add validation for entry amount

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,6 +5,6 @@ class Entry < ApplicationRecord
   belongs_to :activity
   belongs_to :user
   validates :user, :activity, :amount, presence: true
-  validates_length_of :amount, minimum: 1
   validates_numericality_of :amount
+  validates_length_of :amount, minimum: 1
 end
